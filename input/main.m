42476abbf3f5c2f26cb22e54c36e899203bcfcf3
wangchao <wang_chao@corp.netease.com>
2017-03-16 14:46:35 +1300

    Initial Commit

---
+++ input/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  input
+//
+//  Created by Charles on 16/03/17.
+//  Copyright © 2017 Charles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
